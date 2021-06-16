18172200ddedb2f428b95283dbbafddd965613fe
Simon Overell <soverell@gmail.com>
2021-06-16 16:43:58 +0100

    Match the node type to Parcel

--- src/create-cluster.sh
+++ src/create-cluster.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Requires COMPUTE_ZONE and PROJECT_ID to be set.
+
+gcloud config set compute/zone $COMPUTE_ZONE
 gcloud config set project $PROJECT_ID
+gcloud container clusters create test-cluster --num-nodes=1 --machine-type=n1-highmem-8
+
+kubectl get nodes
